f6c0d1f9ce00cb47f673625f04d3678c8b07f578
riyce <riyce@mail.ru>
2022-08-21 01:34:18 +0300

    fixes

--- api/db/kafka.py
+++ api/db/kafka.py
@@ -1,8 +1,12 @@
 import asyncio
+from logging import getLogger
 from typing import Optional
 
 from aiokafka import AIOKafkaProducer
+from aiokafka.errors import KafkaConnectionError
 from core.config import config
+
+logger = getLogger(__name__)
 
 
 class KafkaHandler:
@@ -19,5 +23,13 @@ kafka_handler: Optional[KafkaHandler] = None
 async def get_kafka_handler() -> KafkaHandler:
     loop = asyncio.get_event_loop()
     kafka_producer = AIOKafkaProducer(loop=loop, bootstrap_servers=config.kafka_uri)
-    await kafka_producer.start()
+    try_ = 1
+    while True:
+        try:
+            await kafka_producer.start()
+            break
+        except KafkaConnectionError:
+            logger.error(f"Can`t connect to kafka. Try {try_}")
+            try_ += 1
+            await asyncio.sleep(5)
     return KafkaHandler(kafka_producer)
--- api/users/security.py
+++ api/users/security.py
@@ -1,18 +1,26 @@
+from http import HTTPStatus
+
 import jwt
 from core.config import config
-from fastapi import Security
+from fastapi import HTTPException, Security
 from fastapi.security import HTTPAuthorizationCredentials, HTTPBearer
 
 BEARER = HTTPBearer()
 
 
 async def get_user(user_credentials: HTTPAuthorizationCredentials = Security(BEARER)) -> str:
-    payload = jwt.decode(
-        jwt=user_credentials.credentials,
-        key=config.SECRET_KEY,
-        algorithms=[config.HASH_ALGORITHM],
-        options={"verify_signature": False},
-    )
+    try:
+        payload = jwt.decode(
+            jwt=user_credentials.credentials,
+            key=config.SECRET_KEY,
+            algorithms=[config.HASH_ALGORITHM],
+            options={"verify_signature": False},
+        )
+    except jwt.DecodeError:
+        raise HTTPException(
+            status_code=HTTPStatus.UNAUTHORIZED,
+            detail="Invalid jwt token.",
+        )
     user_data = payload.get("sub")
     user_id = user_data.get("user_id")
     return user_id
