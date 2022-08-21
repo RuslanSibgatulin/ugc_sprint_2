0fb330696224a22228f9b85edfd9588671841d29
riyce <riyce@mail.ru>
2022-08-21 16:31:17 +0300

    little fix

--- api/api/v1/progress.py
+++ api/api/v1/progress.py
@@ -1,5 +1,5 @@
 from fastapi import APIRouter, Depends, Security
-from models.progress import MovieProgress
+from models.progress import MovieProgress, MovieProgressBase
 from services.progress import ProgressService, get_progress_service
 from users.security import get_user
 
@@ -8,9 +8,9 @@ router = APIRouter()
 
 @router.post("/progress")
 async def send_view_progress(
-    movie_progress: MovieProgress,
+    movie_progress: MovieProgressBase,
     event_sender: ProgressService = Depends(get_progress_service),
     user_id: str = Security(get_user),
 ):
-    movie_progress.user_id = user_id
-    await event_sender.send_movie_progress(movie_progress)
+    user_movie_progress = MovieProgress.get_progress(user_id, movie_progress)
+    await event_sender.send_movie_progress(user_movie_progress)
--- api/models/progress.py
+++ api/models/progress.py
@@ -1,8 +1,17 @@
 from pydantic.main import BaseModel
 
 
-class MovieProgress(BaseModel):
+class MovieProgressBase(BaseModel):
     movie_id: str
     time: int
     total_time: int
-    user_id: str = None
+
+
+class MovieProgress(MovieProgressBase):
+    user_id: str
+
+    @classmethod
+    def get_progress(cls, user_id: str, base_progress: MovieProgressBase):
+        payload = base_progress.dict()
+        payload["user_id"] = user_id
+        return cls(**payload)
