ec398f84b7a531afa54f764139f836848d23ae6a
Ruslan Sibgatulin <doolf@yandex.ru>
2022-09-03 14:11:54 +0300

    Fix sentry

--- api/core/sentry_fastapi.py
+++ api/core/sentry_fastapi.py
@@ -1,6 +1,11 @@
+import os
+
 from sentry_sdk import init
 from sentry_sdk.integrations.fastapi import FastApiIntegration
 
 
 def sentry_init():
-    init(integrations=[FastApiIntegration()], traces_sample_rate=1.0)
+    if "SENTRY_DSN" in os.environ.keys():
+        init(
+            integrations=[FastApiIntegration()],
+            traces_sample_rate=os.environ.get("SENTRY_RATE", 0.5))
